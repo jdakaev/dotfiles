7da68333e1034f37b3fdda7147e4bccb0f65853f
jdakaev <jdakaev@disroot.org>
2026-06-02 04:24:23 +0300

    nix: update to 26.05

--- nix/flake.nix
+++ nix/flake.nix
@@ -2,9 +2,9 @@
   description = "nixos flake to setup my hardware";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager/release-25.11";
+    home-manager.url = "github:nix-community/home-manager/release-26.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     lanzaboote.url = "github:nix-community/lanzaboote?tag=v1.0.0";
