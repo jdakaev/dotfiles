d8eaa98ab86a151c50d068a2c0ec929e43c0af60
jdakaev <jdakaev@disroot.org>
2026-06-02 05:31:32 +0300

    neovim: fix after nix update, add a keybind to quickly open up the replace menu

--- .config/nvim/lsp/lua_ls.lua
+++ .config/nvim/lsp/lua_ls.lua
@@ -1,4 +1,4 @@
-vim.lsp.config('lua_ls', {
+return {
   on_init = function(client)
     if client.workspace_folders then
       local path = client.workspace_folders[1].name
@@ -45,4 +45,4 @@ vim.lsp.config('lua_ls', {
   settings = {
     Lua = {}
   }
-})
+}
--- .config/nvim/lua/config/keybinds.lua
+++ .config/nvim/lua/config/keybinds.lua
@@ -1,2 +1,3 @@
 vim.g.mapleader = " "
 vim.keymap.set('n', '<Leader>e', '<cmd>Explore<cr>')
+vim.keymap.set('n', '<leader>s', ':%s//g<left><left>', { noremap = true })
